630bb0e14790e3caf41373d6b988ba9590e9e1d5
fmbah <807966224@qq.com>
2019-05-09 10:35:22 +0800

    mongo db config update

--- eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongoData.java
+++ eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongoData.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,52 +17,24 @@ import java.util.List;
 @EnableConfigurationProperties(MongodbProperties.class)
 public class MongoData {
 
-    //覆盖默认的MongoDbFacotry
-    @Bean
     @Autowired
-    public MongoDbFactory mongoDbFactory(MongodbProperties properties) {
-        //客户端配置（连接数，副本集群验证）
-        MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
-        builder.connectionsPerHost(properties.getMaxConnectionsPerHost());
-        builder.minConnectionsPerHost(properties.getMinConnectionsPerHost());
-        if (properties.getReplicaSet() != null) {
-            builder.requiredReplicaSetName(properties.getReplicaSet());
-        }
+    MongodbProperties config;
+    private MongoData() {}
 
-        builder.threadsAllowedToBlockForConnectionMultiplier(
-                properties.getThreadsAllowedToBlockForConnectionMultiplier());
-        builder.serverSelectionTimeout(properties.getServerSelectionTimeout());
-        builder.maxWaitTime(properties.getMaxWaitTime());
-        builder.maxConnectionIdleTime(properties.getMaxConnectionIdleTime());
-        builder.maxConnectionLifeTime(properties.getMaxConnectionLifeTime());
-        builder.connectTimeout(properties.getConnectTimeout());
-        builder.socketTimeout(properties.getSocketTimeout());
-        builder.sslEnabled(properties.getSslEnabled());
-        builder.sslInvalidHostNameAllowed(properties.getSslInvalidHostNameAllowed());
-        builder.alwaysUseMBeans(properties.getAlwaysUseMBeans());
-        builder.heartbeatFrequency(properties.getHeartbeatFrequency());
-        builder.minHeartbeatFrequency(properties.getMinHeartbeatFrequency());
-        builder.heartbeatConnectTimeout(properties.getHeartbeatConnectTimeout());
-        builder.heartbeatSocketTimeout(properties.getHeartbeatSocketTimeout());
-        builder.localThreshold(properties.getLocalThreshold());
-        MongoClientOptions mongoClientOptions = builder.build();
-        // MongoDB地址列表
-        List<ServerAddress> serverAddresses = new ArrayList<>();
-        for (String address : properties.getAddress()) {
-            String[] hostAndPort = address.split(":");
-            String host = hostAndPort[0];
-            Integer port = Integer.parseInt(hostAndPort[1]);
-            ServerAddress serverAddress = new ServerAddress(host, port);
-            serverAddresses.add(serverAddress);
-        }
-        // 连接认证
-        MongoCredential mongoCredential = MongoCredential.createScramSha1Credential(properties.getUsername(),
-                properties.getAuthenticationDatabase() != null ? properties.getAuthenticationDatabase() : properties.getDatabase(),
-                properties.getPassword().toCharArray());
-        //创建客户端和Factory
-        MongoClient mongoClient = new MongoClient(serverAddresses, mongoCredential, mongoClientOptions);
-        MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongoClient, properties.getDatabase());
-        return mongoDbFactory;
+    @Bean
+    public MongoClient mongoClient() {
+        List<ServerAddress> addresses = new ArrayList<>();
+        MongoCredential credential = MongoCredential.createCredential(
+                config.getUsername(), config.getDatabase(), config.getPassword().toCharArray());
+        addresses.add(new ServerAddress(config.getHost(), Integer.parseInt(config.getPort())));
+        MongoClient mongoClient = new MongoClient(addresses, credential, MongoClientOptions.builder().build());
+        return mongoClient;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient(), config.getDatabase());
+        return mongoTemplate;
     }
 
 }
--- eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongodbProperties.java
+++ eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongodbProperties.java
@@ -2,67 +2,26 @@ package com.jxbig.sharp.eurekaclient.common.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import java.util.List;
-
 @ConfigurationProperties(prefix = MongodbProperties.MON, ignoreUnknownFields = false)
 public class MongodbProperties {
 
     //对应配置文件里的配置键
-    public final static String MON = "mongo";
+    public final static String MON = "mon";
 
-    @NotBlank
+    public static String getMon() {
+        return MON;
+    }
+
     private String database;
-    @NotEmpty
-    private List<String> address;
-    private String replicaSet;
+
+    private String host;
+
+    private String port;
+
     private String username;
+
     private String password;
-    private Integer minConnectionsPerHost = 0;
-    private Integer maxConnectionsPerHost = 100;
-    private Integer threadsAllowedToBlockForConnectionMultiplier = 5;
-    private Integer serverSelectionTimeout = 30000;
-    private Integer maxWaitTime = 120000;
-    private Integer maxConnectionIdleTime = 0;
-    private Integer maxConnectionLifeTime = 0;
-    private Integer connectTimeout = 10000;
-    private Integer socketTimeout = 0;
-    private Boolean socketKeepAlive = false;
-    private Boolean sslEnabled = false;
-    private Boolean sslInvalidHostNameAllowed = false;
-    private Boolean alwaysUseMBeans = false;
-    private Integer heartbeatFrequency = 10000;
-    private Integer minHeartbeatFrequency = 500;
-    private Integer heartbeatConnectTimeout = 20000;
-    private Integer heartbeatSocketTimeout = 20000;
-    private Integer localThreshold = 15;
-    private String authenticationDatabase;
 
-
-    public String getDatabase() {
-        return database;
-    }
-
-    public void setDatabase(String database) {
-        this.database = database;
-    }
-
-    public List<String> getAddress() {
-        return address;
-    }
-
-    public void setAddress(List<String> address) {
-        this.address = address;
-    }
-
-    public String getReplicaSet() {
-        return replicaSet;
-    }
-
-    public void setReplicaSet(String replicaSet) {
-        this.replicaSet = replicaSet;
-    }
 
     public String getUsername() {
         return username;
@@ -80,155 +39,28 @@ public class MongodbProperties {
         this.password = password;
     }
 
-    public Integer getMinConnectionsPerHost() {
-        return minConnectionsPerHost;
+
+    public String getDatabase() {
+        return database;
     }
 
-    public void setMinConnectionsPerHost(Integer minConnectionsPerHost) {
-        this.minConnectionsPerHost = minConnectionsPerHost;
+    public void setDatabase(String database) {
+        this.database = database;
     }
 
-    public Integer getMaxConnectionsPerHost() {
-        return maxConnectionsPerHost;
+    public String getHost() {
+        return host;
     }
 
-    public void setMaxConnectionsPerHost(Integer maxConnectionsPerHost) {
-        this.maxConnectionsPerHost = maxConnectionsPerHost;
+    public void setHost(String host) {
+        this.host = host;
     }
 
-    public Integer getThreadsAllowedToBlockForConnectionMultiplier() {
-        return threadsAllowedToBlockForConnectionMultiplier;
+    public String getPort() {
+        return port;
     }
 
-    public void setThreadsAllowedToBlockForConnectionMultiplier(Integer threadsAllowedToBlockForConnectionMultiplier) {
-        this.threadsAllowedToBlockForConnectionMultiplier = threadsAllowedToBlockForConnectionMultiplier;
-    }
-
-    public Integer getServerSelectionTimeout() {
-        return serverSelectionTimeout;
-    }
-
-    public void setServerSelectionTimeout(Integer serverSelectionTimeout) {
-        this.serverSelectionTimeout = serverSelectionTimeout;
-    }
-
-    public Integer getMaxWaitTime() {
-        return maxWaitTime;
-    }
-
-    public void setMaxWaitTime(Integer maxWaitTime) {
-        this.maxWaitTime = maxWaitTime;
-    }
-
-    public Integer getMaxConnectionIdleTime() {
-        return maxConnectionIdleTime;
-    }
-
-    public void setMaxConnectionIdleTime(Integer maxConnectionIdleTime) {
-        this.maxConnectionIdleTime = maxConnectionIdleTime;
-    }
-
-    public Integer getMaxConnectionLifeTime() {
-        return maxConnectionLifeTime;
-    }
-
-    public void setMaxConnectionLifeTime(Integer maxConnectionLifeTime) {
-        this.maxConnectionLifeTime = maxConnectionLifeTime;
-    }
-
-    public Integer getConnectTimeout() {
-        return connectTimeout;
-    }
-
-    public void setConnectTimeout(Integer connectTimeout) {
-        this.connectTimeout = connectTimeout;
-    }
-
-    public Integer getSocketTimeout() {
-        return socketTimeout;
-    }
-
-    public void setSocketTimeout(Integer socketTimeout) {
-        this.socketTimeout = socketTimeout;
-    }
-
-    public Boolean getSocketKeepAlive() {
-        return socketKeepAlive;
-    }
-
-    public void setSocketKeepAlive(Boolean socketKeepAlive) {
-        this.socketKeepAlive = socketKeepAlive;
-    }
-
-    public Boolean getSslEnabled() {
-        return sslEnabled;
-    }
-
-    public void setSslEnabled(Boolean sslEnabled) {
-        this.sslEnabled = sslEnabled;
-    }
-
-    public Boolean getSslInvalidHostNameAllowed() {
-        return sslInvalidHostNameAllowed;
-    }
-
-    public void setSslInvalidHostNameAllowed(Boolean sslInvalidHostNameAllowed) {
-        this.sslInvalidHostNameAllowed = sslInvalidHostNameAllowed;
-    }
-
-    public Boolean getAlwaysUseMBeans() {
-        return alwaysUseMBeans;
-    }
-
-    public void setAlwaysUseMBeans(Boolean alwaysUseMBeans) {
-        this.alwaysUseMBeans = alwaysUseMBeans;
-    }
-
-    public Integer getHeartbeatFrequency() {
-        return heartbeatFrequency;
-    }
-
-    public void setHeartbeatFrequency(Integer heartbeatFrequency) {
-        this.heartbeatFrequency = heartbeatFrequency;
-    }
-
-    public Integer getMinHeartbeatFrequency() {
-        return minHeartbeatFrequency;
-    }
-
-    public void setMinHeartbeatFrequency(Integer minHeartbeatFrequency) {
-        this.minHeartbeatFrequency = minHeartbeatFrequency;
-    }
-
-    public Integer getHeartbeatConnectTimeout() {
-        return heartbeatConnectTimeout;
-    }
-
-    public void setHeartbeatConnectTimeout(Integer heartbeatConnectTimeout) {
-        this.heartbeatConnectTimeout = heartbeatConnectTimeout;
-    }
-
-    public Integer getHeartbeatSocketTimeout() {
-        return heartbeatSocketTimeout;
-    }
-
-    public void setHeartbeatSocketTimeout(Integer heartbeatSocketTimeout) {
-        this.heartbeatSocketTimeout = heartbeatSocketTimeout;
-    }
-
-    public Integer getLocalThreshold() {
-        return localThreshold;
-    }
-
-    public void setLocalThreshold(Integer localThreshold) {
-        this.localThreshold = localThreshold;
-    }
-
-    public String getAuthenticationDatabase() {
-        return authenticationDatabase;
-    }
-
-    public void setAuthenticationDatabase(String authenticationDatabase) {
-        this.authenticationDatabase = authenticationDatabase;
+    public void setPort(String port) {
+        this.port = port;
     }
 }
