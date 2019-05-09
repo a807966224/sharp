3753bd4a28d8bef6ba3b9776d0c9b742fe765f29
fmbah <807966224@qq.com>
2019-05-09 10:05:57 +0800

    mongo db config update

--- eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongodbProperties.java
+++ eureka-client/src/main/java/com/jxbig/sharp/eurekaclient/common/config/MongodbProperties.java
@@ -2,26 +2,67 @@ package com.jxbig.sharp.eurekaclient.common.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
 @ConfigurationProperties(prefix = MongodbProperties.MON, ignoreUnknownFields = false)
 public class MongodbProperties {
 
     //对应配置文件里的配置键
-    public final static String MON = "mon";
+    public final static String MON = "mongo";
 
-    public static String getMon() {
-        return MON;
+    @NotBlank
+    private String database;
+    @NotEmpty
+    private List<String> address;
+    private String replicaSet;
+    private String username;
+    private String password;
+    private Integer minConnectionsPerHost = 0;
+    private Integer maxConnectionsPerHost = 100;
+    private Integer threadsAllowedToBlockForConnectionMultiplier = 5;
+    private Integer serverSelectionTimeout = 30000;
+    private Integer maxWaitTime = 120000;
+    private Integer maxConnectionIdleTime = 0;
+    private Integer maxConnectionLifeTime = 0;
+    private Integer connectTimeout = 10000;
+    private Integer socketTimeout = 0;
+    private Boolean socketKeepAlive = false;
+    private Boolean sslEnabled = false;
+    private Boolean sslInvalidHostNameAllowed = false;
+    private Boolean alwaysUseMBeans = false;
+    private Integer heartbeatFrequency = 10000;
+    private Integer minHeartbeatFrequency = 500;
+    private Integer heartbeatConnectTimeout = 20000;
+    private Integer heartbeatSocketTimeout = 20000;
+    private Integer localThreshold = 15;
+    private String authenticationDatabase;
+
+
+    public String getDatabase() {
+        return database;
     }
 
-    private String database;
+    public void setDatabase(String database) {
+        this.database = database;
+    }
 
-    private String host;
+    public List<String> getAddress() {
+        return address;
+    }
 
-    private String port;
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
 
-    private String username;
+    public String getReplicaSet() {
+        return replicaSet;
+    }
 
-    private String password;
-
+    public void setReplicaSet(String replicaSet) {
+        this.replicaSet = replicaSet;
+    }
 
     public String getUsername() {
         return username;
@@ -39,28 +80,155 @@ public class MongodbProperties {
         this.password = password;
     }
 
-
-    public String getDatabase() {
-        return database;
+    public Integer getMinConnectionsPerHost() {
+        return minConnectionsPerHost;
     }
 
-    public void setDatabase(String database) {
-        this.database = database;
+    public void setMinConnectionsPerHost(Integer minConnectionsPerHost) {
+        this.minConnectionsPerHost = minConnectionsPerHost;
     }
 
-    public String getHost() {
-        return host;
+    public Integer getMaxConnectionsPerHost() {
+        return maxConnectionsPerHost;
     }
 
-    public void setHost(String host) {
-        this.host = host;
+    public void setMaxConnectionsPerHost(Integer maxConnectionsPerHost) {
+        this.maxConnectionsPerHost = maxConnectionsPerHost;
     }
 
-    public String getPort() {
-        return port;
+    public Integer getThreadsAllowedToBlockForConnectionMultiplier() {
+        return threadsAllowedToBlockForConnectionMultiplier;
     }
 
-    public void setPort(String port) {
-        this.port = port;
+    public void setThreadsAllowedToBlockForConnectionMultiplier(Integer threadsAllowedToBlockForConnectionMultiplier) {
+        this.threadsAllowedToBlockForConnectionMultiplier = threadsAllowedToBlockForConnectionMultiplier;
+    }
+
+    public Integer getServerSelectionTimeout() {
+        return serverSelectionTimeout;
+    }
+
+    public void setServerSelectionTimeout(Integer serverSelectionTimeout) {
+        this.serverSelectionTimeout = serverSelectionTimeout;
+    }
+
+    public Integer getMaxWaitTime() {
+        return maxWaitTime;
+    }
+
+    public void setMaxWaitTime(Integer maxWaitTime) {
+        this.maxWaitTime = maxWaitTime;
+    }
+
+    public Integer getMaxConnectionIdleTime() {
+        return maxConnectionIdleTime;
+    }
+
+    public void setMaxConnectionIdleTime(Integer maxConnectionIdleTime) {
+        this.maxConnectionIdleTime = maxConnectionIdleTime;
+    }
+
+    public Integer getMaxConnectionLifeTime() {
+        return maxConnectionLifeTime;
+    }
+
+    public void setMaxConnectionLifeTime(Integer maxConnectionLifeTime) {
+        this.maxConnectionLifeTime = maxConnectionLifeTime;
+    }
+
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Integer getSocketTimeout() {
+        return socketTimeout;
+    }
+
+    public void setSocketTimeout(Integer socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public Boolean getSocketKeepAlive() {
+        return socketKeepAlive;
+    }
+
+    public void setSocketKeepAlive(Boolean socketKeepAlive) {
+        this.socketKeepAlive = socketKeepAlive;
+    }
+
+    public Boolean getSslEnabled() {
+        return sslEnabled;
+    }
+
+    public void setSslEnabled(Boolean sslEnabled) {
+        this.sslEnabled = sslEnabled;
+    }
+
+    public Boolean getSslInvalidHostNameAllowed() {
+        return sslInvalidHostNameAllowed;
+    }
+
+    public void setSslInvalidHostNameAllowed(Boolean sslInvalidHostNameAllowed) {
+        this.sslInvalidHostNameAllowed = sslInvalidHostNameAllowed;
+    }
+
+    public Boolean getAlwaysUseMBeans() {
+        return alwaysUseMBeans;
+    }
+
+    public void setAlwaysUseMBeans(Boolean alwaysUseMBeans) {
+        this.alwaysUseMBeans = alwaysUseMBeans;
+    }
+
+    public Integer getHeartbeatFrequency() {
+        return heartbeatFrequency;
+    }
+
+    public void setHeartbeatFrequency(Integer heartbeatFrequency) {
+        this.heartbeatFrequency = heartbeatFrequency;
+    }
+
+    public Integer getMinHeartbeatFrequency() {
+        return minHeartbeatFrequency;
+    }
+
+    public void setMinHeartbeatFrequency(Integer minHeartbeatFrequency) {
+        this.minHeartbeatFrequency = minHeartbeatFrequency;
+    }
+
+    public Integer getHeartbeatConnectTimeout() {
+        return heartbeatConnectTimeout;
+    }
+
+    public void setHeartbeatConnectTimeout(Integer heartbeatConnectTimeout) {
+        this.heartbeatConnectTimeout = heartbeatConnectTimeout;
+    }
+
+    public Integer getHeartbeatSocketTimeout() {
+        return heartbeatSocketTimeout;
+    }
+
+    public void setHeartbeatSocketTimeout(Integer heartbeatSocketTimeout) {
+        this.heartbeatSocketTimeout = heartbeatSocketTimeout;
+    }
+
+    public Integer getLocalThreshold() {
+        return localThreshold;
+    }
+
+    public void setLocalThreshold(Integer localThreshold) {
+        this.localThreshold = localThreshold;
+    }
+
+    public String getAuthenticationDatabase() {
+        return authenticationDatabase;
+    }
+
+    public void setAuthenticationDatabase(String authenticationDatabase) {
+        this.authenticationDatabase = authenticationDatabase;
     }
 }
